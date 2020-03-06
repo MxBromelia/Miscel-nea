bc3fb6d2789860710251bd21081bd686128a7f1e
MxBromelia <ruanmacambira@gmail.com>
2020-03-06 13:28:05 -0300

    TDD by Example - Capítulo 11

--- Ruby/tdd-beck-part-1/spec/money/money_spec.rb
+++ Ruby/tdd-beck-part-1/spec/money/money_spec.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 describe Money do
-  it 'compara duas moedas de valores diferentes' do
-    expect(Money.franc(5)).to_not eq(Money.dollar(5))
+  describe '#times(*)' do
+    it 'mutiplica os escalares e retorna um objeto com o produto como valor' do
+      expect(Money.dollar(5) * 3).to eq(Money.dollar(15))
+    end
+  end
+
+  describe '#equal(==)' do
+    it 'comparar valores se moeda é a mesma' do
+      expect(Money.franc(5)).to eq(Money.franc(5))
+      expect(Money.dollar(5)).to_not eq(Money.dollar(6))
+    end
+
+    it 'comparar valores com peso se moeda é outra' do
+      expect(Money.franc(5)).to_not eq(Money.dollar(5))
+    end
+  end
+
+  describe '#currency' do
+    it 'comparara a igualdade do tipo de moeda' do
+      expect(Money.franc(5).currency).to eq('CHF')
+      expect(Money.dollar(5).currency).to eq('USD')
+    end
   end
 end
