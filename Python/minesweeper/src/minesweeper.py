467f92ad5fcf2a08a7a5c1da756593179a0137e4
MxBromelia <ruanmacambira@gmail.com>
2020-03-23 19:52:46 -0300

    refatoração: Separar elementos do campo do objeto do jogo

--- Python/minesweeper/src/minesweeper.py
+++ Python/minesweeper/src/minesweeper.py
@@ -1,38 +1,83 @@
 from random import randint
+from enum import Enum
 
-class Minesweeper:
+class FieldEnum(Enum):
     NOTHING = 0
     MINE = -1
     ONE_AROUND = 1
+
+class Field:
+    def __init__(self, rows, cols, mines):
+        self.__rows = rows
+        self.__cols = cols
+        self.__cells = self.rows * self.cols
+        self.__mines = mines
+        self.__table = [FieldEnum.NOTHING for _ in range(self.cells)]
+        self.__dug_cells = 0
+        self.__dug = [False for _ in range(self.cells)]
+
+    def all_dug(self):
+        return self.__dug_cells == self.cells
+
+    def index(self, row, col):
+        return self.cols*row + col
+
+    def dig(self, row, col):
+        if self.dug[self.index(row, col)]:
+            return False
+
+        self.__dug_cells += 1
+        self.dug[self.index(row, col)] = True
+        return True
+
+    @property
+    def rows(self):
+        return self.__rows
+
+    @property
+    def cols(self):
+        return self.__cols
+
+    @property
+    def cells(self):
+        return self.__cells
+
+    @property
+    def mines(self):
+        return self.__mines
+
+    @property
+    def dug(self):
+        return self.__dug
+
+    def __getitem__(self, key):
+        return self.__table[key*self.cols:(key+1)*self.cols]
+
+    def __setitem__(self, key, value):
+        self.__table[key] = value
+
+
+class Minesweeper:
+    """ Campo Minado """
 
     DEFEAT = 0
     VICTORY = 1
     UNFINISHED = 2
 
     def __init__(self, rows, cols, mines, mine_generator=None):
-        self.rows = rows
-        self.cols = cols
-        self.mines = mines
-        self.__cells = self.rows * self.cols
-        self.dug_cells = 0
-        self.dug = [False for _ in range(self.cells)]
-        self.__status = self.UNFINISHED
         self.__mine_generator = mine_generator or self.__generate_mines
-        self.mine_field = self.__generate_mine_field__()
+        self.mine_field = self.__generate_mine_field__(rows, cols, mines)
+        self.__status = self.UNFINISHED
 
     def dig(self, row, col):
-        cell_index = self.cols*row + col
-        cell = self.mine_field[cell_index]
+        self.mine_field.dig(row, col)
+        cell = self.mine_field[row][col]
 
-        if not self.dug[cell_index]:
-            self.dug_cells += 1
-            self.dug[cell_index] = True
-
-        if cell == self.MINE:
+        if cell == FieldEnum.MINE:
             self.__status = self.DEFEAT
             return cell
 
-        if self.cells == self.dug_cells:
+        if self.mine_field.all_dug:
             self.__status = self.VICTORY
 
         return cell
@@ -41,20 +86,18 @@ class Minesweeper:
     def status(self):
         return self.__status
 
-    @property
-    def cells(self):
-        return self.rows * self.cols
-
     def __generate_mines(self, cells, mines):
         return [randint(0, cells-1) for _ in range(mines)]
 
-    def __generate_mine_field__(self):
-        mine_field = [self.NOTHING for _ in range(self.cells)]
-        for index in self.__mine_generator(self.cells, self.mines):
-            mine_field[index] = self.MINE
+    def __generate_mine_field__(self, rows, cols, mines):
+        mine_field = Field(rows, cols, mines)
+        for index in self.__mine_generator(mine_field.cells, mines):
+            mine_field[index] = FieldEnum.MINE
 
-        for index in range(self.cells-1):
-            if mine_field[index+1] == self.MINE:
-                mine_field[index] = self.ONE_AROUND
+        for row in range(mine_field.rows):
+            for col in range(mine_field.cols-1):
+                if mine_field[row][col+1] == FieldEnum.MINE:
+                    # mine_field[row][col] = FieldEnum.ONE_AROUND
+                    mine_field[mine_field.index(row, col)] = FieldEnum.ONE_AROUND
 
         return mine_field
--- Python/minesweeper/test/minesweeper_test.py
+++ Python/minesweeper/test/minesweeper_test.py
@@ -1,30 +1,32 @@
 from unittest import TestCase
-from src.minesweeper import Minesweeper
+from src.minesweeper import Minesweeper, FieldEnum, Field
+
+class TestField(TestCase):
+    def test_digAHoleSuccessfully(self):
+        field = Field(1, 1, 1)
+        self.assertTrue(field.dig(0, 0))
+
+    def test_digAHoleTwiceFails(self):
+        field = Field(1, 1, 1)
+        field.dig(0, 0)
+        self.assertFalse(field.dig(0, 0))
+
+    def test_getFieldElement(self):
+        field = Field(1, 1, 1)
+        self.assertEqual(field[0][0], FieldEnum.NOTHING)
 
 class TestMinesweeper(TestCase):
-
-    def test_digAHoleIncreasesDugCells(self):
-        mine = Minesweeper(1, 1, 0)
-        mine.dig(0, 0)
-        self.assertEqual(mine.dug_cells, 1)
-
-    def test_aHoleCannotBeDugTwice(self):
-        mine = Minesweeper(1, 1, 0)
-        mine.dig(0, 0)
-        mine.dig(0, 0)
-        self.assertEqual(mine.dug_cells, 1)
-
     def test_digAnEmptyHoleRevealsNothing(self):
         mine = Minesweeper(1, 1, 0)
-        self.assertEqual(mine.dig(0, 0), Minesweeper.NOTHING)
+        self.assertEqual(mine.dig(0, 0), FieldEnum.NOTHING)
 
     def test_digAHoleWithMinesRevealsIt(self):
         mine = Minesweeper(1, 1, 1)
-        self.assertEqual(mine.dig(0, 0), Minesweeper.MINE)
+        self.assertEqual(mine.dig(0, 0), FieldEnum.MINE)
 
     def test_digAHoleWithOneMineAroundShowsIt(self):
         mine = Minesweeper(1, 2, 1, lambda _x, _y: [1])
-        self.assertEqual(mine.dig(0, 0), Minesweeper.ONE_AROUND)
+        self.assertEqual(mine.dig(0, 0), FieldEnum.ONE_AROUND)
 
     def test_startCondition(self):
         mine = Minesweeper(1, 1, 1)
